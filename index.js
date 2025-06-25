31a926fa6e85ef5d07908ad9e17de5638dd95cd6
sathosoe <121236912+sathosoe@users.noreply.github.com>
2025-06-25 17:02:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.post('/chat', async (req, res) => {
     const prompt = req.body.prompt; // Noaアプリから送られるプロンプト
 
     const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-      model: 'gpt-4', // ここでモデルを指定（gpt-4でも可）
+      model: 'gpt-4o', // ここでモデルを指定（gpt-4でも可）
       messages: [
         { role: 'system', content: "You are Noa, a smart and witty personal AI assistant inside the user's AR smart glasses that answers all user queries and questions" },
         { role: 'user', content: prompt }
